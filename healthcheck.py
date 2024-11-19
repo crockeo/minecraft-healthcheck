d569a59560a41dc104cd83392deb2406528bded9
Cerek Hillen <cerekh@gmail.com>
2024-11-19 14:26:47 -0800

    make a healthcheck work!

--- healthcheck.py
+++ healthcheck.py
@@ -1,15 +1,25 @@
 import logging
 import os
+import random
+import secrets
 import signal
 import socket
+import struct
 import time
 from dataclasses import dataclass
+from io import BytesIO
 from threading import Event, Thread
 from typing import Callable, Self
 
 import prometheus_client
 
+logging.basicConfig()
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Magic value defined in the RakNet protocol.
+# Don't ask me where it's from...
+MAGIC = bytes.fromhex("00ffff00fefefefefdfdfdfd12345678")
 
 
 @dataclass
@@ -55,26 +65,65 @@ class HealthcheckResult:
         self.unhealthy_gauge.set(1)
 
 
+def build_ping_packet() -> bytes:
+    buf = BytesIO()
+    buf.write(b"\x01")
+    buf.write(int(time.time()).to_bytes(8, "big"))
+    buf.write(MAGIC)
+    buf.write(random.randbytes(8))
+    return buf.getvalue()
+
+
+def create_unconnected_ping_frame(start_time: int):
+    packet = bytearray(33)
+    packet[0] = 0x01
+    struct.pack_into("<Q", packet, 1, int((time.time() * 1000) - start_time))
+    packet[9:25] = MAGIC
+    packet[25:33] = secrets.token_bytes(8)
+    return bytes(packet)
+
+
+def ping_bedrock(environ: Environ, start_time: int, timeout: float = 1.0) -> bool:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(timeout)
+
+    try:
+        ping_packet = create_unconnected_ping_frame(start_time)
+        sock.sendto(ping_packet, (environ.minecraft_host, environ.minecraft_port))
+
+        pong_packet, _ = sock.recvfrom(1024)
+        if pong_packet[0] == 0x1C:
+            return True
+        return False
+    finally:
+        sock.close()
+
+
 def ping_minecraft_server_main(environ: Environ, shutdown: Event) -> None:
     healthcheck_result = HealthcheckResult("minecraft:healthcheck")
+    start_time = int(time.time() * 1000)
     time_since_last_ping = 0.0
     last_time = time.monotonic()
     while not shutdown.is_set():
         now = time.monotonic()
-        time_since_last_ping += now - last_time
+        time_since_last_ping -= now - last_time
         last_time = now
 
-        if time_since_last_ping > 5.0:
-            time_since_last_ping = 0.0
-            logger.info("Attempting healthcheck")
+        if time_since_last_ping <= 0.0:
+            time_since_last_ping = 1.0
+            logger.info(
+                f"Attempting healthcheck {environ.minecraft_host}:{environ.minecraft_port}"
+            )
             healthcheck_result.mark_attempt()
             try:
-                sock = socket.socket(socket.AF_INET)
-                sock.connect((environ.minecraft_host, environ.minecraft_port))
-                sock.close()
+                healthy = ping_bedrock(environ, start_time)
+            except ConnectionRefusedError:
+                healthy = False
+
+            if healthy:
                 logger.info("Healthcheck succeeded")
                 healthcheck_result.mark_healthy()
-            except ConnectionRefusedError:
+            else:
                 logger.error("Healthcheck failed", exc_info=True)
                 healthcheck_result.mark_unhealthy()
 
@@ -83,7 +132,10 @@ def ping_minecraft_server_main(environ: Environ, shutdown: Event) -> None:
 
 def signal_handler(shutdown: Event) -> Callable[..., None]:
     def _signal_handler(*args, **kwargs):
-        print("Shutting down...")
+        if shutdown.is_set():
+            exit(1)
+
+        logger.info("Shutting down...")
         shutdown.set()
 
     return _signal_handler
@@ -110,11 +162,11 @@ def main() -> None:
 
     shutdown.wait()
 
-    print("Shutting down prometheus server...")
+    logger.info("Shutting down prometheus server...")
     server.shutdown()
     server_thread.join()
 
-    print("Shutting down healthcheck thread...")
+    logger.info("Shutting down healthcheck thread...")
     healthcheck_thread.join()
 
 
